bd990d02a186579fa35de9d83cf10185fcd159a2
rabinniroula <rabinniroula99@gmail.com>
2023-04-21 09:22:17 +0545

    working prototype

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
 import db, auth
 
 app = FastAPI()
 
 
-@app.get("/login/")
-async def login():
-    pass
+@app.post("/login/")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    token = auth.getToken(username=form_data.username, password=form_data.password)
+    if token != False:
+        return {'access_token': token, 'token_type': 'bearer'}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
